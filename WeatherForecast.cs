d272b74659541ccbf9a3592e7bfdf97bab9545fe
Jamie Nicholls <jnicholl@massey.ac.nz>
2021-12-01 11:02:36 +1300

    Init

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace blast_off
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
